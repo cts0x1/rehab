bbbd302289d93c2150f141d4495a9e04a44cbb23
cts <98242496+cts0x1@users.noreply.github.com>
2022-08-19 22:29:39 +0100

    update for firefox compatibility
    chromium versions worked fine, firefox now should be functional (hopefully)

--- I Still Hate The New Rehab Button.user.js
+++ I Still Hate The New Rehab Button.user.js
@@ -1,17 +1,25 @@
 // ==UserScript==
 // @name         I Still Hate The New Rehab Button
 // @namespace    torn.com
-// @version      0.5
+// @version      0.6
 // @description  Who seriously thought moving the rehab button was a good idea?
 // @author       cts [2621827]
 // @match        *.torn.com/index.php*
+// @match        *.torn.com/hospitalview.php*
 // ==/UserScript==
 
 $(window).load(function() {
     if ($('h4#skip-to-content:contains(Switzerland)').length) {
         let rehabilitation = $('a:contains(Rehabilitation)');
         $(rehabilitation).appendTo($('#top-page-links-list'));
-        $(rehabilitation).addClass('right line-h24');
+        $(rehabilitation).addClass('line-h24 last right');
+        $(rehabilitation).css('textDecoration', 'none');
+    }
+    // mostly for debug because I got hosp'd in the process of fixing, leaving it here though because I can
+    if ($('h4#skip-to-content:contains(Swiss Hospital)').length) {
+        let rehabilitation = $('a:contains(Rehabilitation)');
+        $(rehabilitation).appendTo($('#top-page-links-list'));
+        $(rehabilitation).addClass('line-h24 last right');
         $(rehabilitation).css('textDecoration', 'none');
     }
 });
