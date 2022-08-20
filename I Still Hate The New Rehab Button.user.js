7afcb748ad9fbe937d8bfd6e35e03d9b232d224f
cts <98242496+cts0x1@users.noreply.github.com>
2022-08-20 18:55:40 +0100

    Update I Still Hate The New Rehab Button.user.js

--- I Still Hate The New Rehab Button.user.js
+++ I Still Hate The New Rehab Button.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         I Still Hate The New Rehab Button
 // @namespace    torn.com
-// @version      0.6
+// @version      0.7
 // @description  Who seriously thought moving the rehab button was a good idea?
 // @author       cts [2621827]
 // @match        *.torn.com/index.php*
@@ -12,14 +12,14 @@ $(window).load(function() {
     if ($('h4#skip-to-content:contains(Switzerland)').length) {
         let rehabilitation = $('a:contains(Rehabilitation)');
         $(rehabilitation).appendTo($('#top-page-links-list'));
-        $(rehabilitation).addClass('line-h24 last right');
+        $(rehabilitation).addClass('t-clear h c-pointer  line-h24 right last'); //added more to class in hopes that works
         $(rehabilitation).css('textDecoration', 'none');
     }
     // mostly for debug because I got hosp'd in the process of fixing, leaving it here though because I can
     if ($('h4#skip-to-content:contains(Swiss Hospital)').length) {
         let rehabilitation = $('a:contains(Rehabilitation)');
         $(rehabilitation).appendTo($('#top-page-links-list'));
-        $(rehabilitation).addClass('line-h24 last right');
+        $(rehabilitation).addClass('t-clear h c-pointer  line-h24 right last');
         $(rehabilitation).css('textDecoration', 'none');
     }
 });
